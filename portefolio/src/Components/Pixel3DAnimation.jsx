28e1fd97ee89963fcdf7f2f52b83d60e25c72500
w0lwin <teo.bacher@hotmail.com>
2023-12-29 11:49:21 +0100

    feat(Controller) theme

--- portefolio/src/Components/Pixel3DAnimation.jsx
+++ portefolio/src/Components/Pixel3DAnimation.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 class Pixel3D {
-  constructor(x, y, z) {
+  constructor(x, y, z, color) {
     this.x = x;
     this.y = y;
     this.z = z;
+    this.color = color;
   }
 }
 
 const Pixel3DAnimation = ({ isDarkTheme }) => {
-  const [isAnimationActive, setIsAnimationActive] = useState(false);
   const canvasRef = useRef(null);
   const offsetX = useRef(0);
   const offsetY = useRef(0);
   const mouseX = useRef(0);
   const mouseY = useRef(0);
+
   const width = window.innerWidth;
   const height = window.innerHeight;
   const halfWidth = width / 2;
@@ -28,59 +29,39 @@ const Pixel3DAnimation = ({ isDarkTheme }) => {
     canvas.width = width;
     canvas.height = height;
 
+    const defaultColor = isDarkTheme ? [255, 255, 255] : [0, 0, 0];
     const pixels = [];
 
-    for (let x = -1000; x < 1000; x += 5) {
-      for (let z = -1000; z < 1000; z += 5) {
+    for (let x = -1000; x < 1000; x += 10) {
+      for (let z = -1000; z < 1000; z += 10) {
         const zOscillation = Math.sin(z * (Math.PI * 4 / 500));
         const xOscillation = Math.sin((x + z) * (Math.PI * 2 / 500));
-        const pixel = new Pixel3D(x, (zOscillation + xOscillation) * 14 + 30, z);
+        const pixel = new Pixel3D(x, (zOscillation + xOscillation) * 14 + 30, z, defaultColor);
         pixels.push(pixel);
       }
     }
 
     const render = () => {
-      if (!isAnimationActive) {
-        // Ne pas rendre l'animation si elle n'est pas activée
-        return;
-      }
-
       offsetX.current += (mouseX.current - offsetX.current) * 0.1;
       offsetY.current += (mouseY.current - offsetY.current) * 0.1;
 
       context.clearRect(0, 0, width, height);
-      const imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
 
-      let i = pixels.length;
-      while (i--) {
-        const pixel = pixels[i];
+      pixels.forEach(pixel => {
         const scale = fov / (fov + pixel.z);
         const x2d = ((pixel.x + offsetX.current) * scale) + halfWidth;
         const y2d = ((pixel.y + offsetY.current + 30) * scale) + halfHeight;
-        let color = [255, 255, 255];
-
-        // if (isDarkTheme) {
-        //   color = [0, 0, 0];
-        // }
-
         const alpha = 255 - pixel.z - (0.65 * 255);
-        setPixel(imagedata, x2d, y2d, ...color, alpha);
+        context.fillStyle = `rgba(${pixel.color[0]}, ${pixel.color[1]}, ${pixel.color[2]}, ${alpha / 255})`;
+        context.fillRect(x2d, y2d, 1, 1);
         pixel.z -= 1;
+        if (pixel.z < -fov) {
+          pixel.z += (fov * 2);
+          pixel.color = isDarkTheme ? [255, 255, 255] : [0, 0, 0];
+        }
+      });
 
-        if (pixel.z < -fov) pixel.z += (fov * 2);
-      }
-
-      context.putImageData(imagedata, 0, 0);
       requestAnimationFrame(render);
-    };
-
-    const setPixel = (imagedata, x, y, r, g, b, a) => {
-      if ((x < 0) || (x > width) || (y < 0) || (y > width)) return;
-      const i = ((y >> 0) * imagedata.width + (x >> 0)) * 4;
-      imagedata.data[i] = r;
-      imagedata.data[i + 1] = g;
-      imagedata.data[i + 2] = b;
-      imagedata.data[i + 3] = a;
     };
 
     const onMouseMove = (e) => {
@@ -91,16 +72,10 @@ const Pixel3DAnimation = ({ isDarkTheme }) => {
     document.body.addEventListener("mousemove", onMouseMove);
     render();
 
-    // Nettoyez l'écouteur d'événements lors du démontage du composant
     return () => {
       document.body.removeEventListener("mousemove", onMouseMove);
     };
-  }, [width, height, halfWidth, halfHeight, isDarkTheme, isAnimationActive]);
-
-  // Mettez à jour l'état de l'animation lorsque le thème sombre change
-  useEffect(() => {
-    setIsAnimationActive(isDarkTheme);
-  }, [isDarkTheme]);
+  }, [width, height, halfWidth, halfHeight, isDarkTheme]);
 
   return <canvas ref={canvasRef} />;
 };
