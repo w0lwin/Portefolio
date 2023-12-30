42e277a6cbec5343f8198403892c55211f83788e
w0lwin <teo.bacher@hotmail.com>
2023-12-30 10:20:20 +0100

    feat(controller) three.js

--- portefolio/src/Components/Pixel3DAnimation.jsx
+++ portefolio/src/Components/Pixel3DAnimation.jsx
@@ -1,83 +1,54 @@
-import React, { useEffect, useRef } from 'react';
-
-class Pixel3D {
-  constructor(x, y, z, color) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.color = color;
-  }
-}
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
 
 const Pixel3DAnimation = ({ isDarkTheme }) => {
-  const canvasRef = useRef(null);
-  const offsetX = useRef(0);
-  const offsetY = useRef(0);
-  const mouseX = useRef(0);
-  const mouseY = useRef(0);
-
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  const halfWidth = width / 2;
-  const halfHeight = height / 2;
-  const fov = 250;
+  const mountRef = useRef(null);
+  const particles = new Array();
+  const particleMaterial = new THREE.PointsMaterial({ size: 1, color: isDarkTheme ? 0xffffff : 0x000000 });
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
+    // Création de la scène et de la caméra
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    canvas.width = width;
-    canvas.height = height;
+    // Création du renderer avec fond transparent
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    const defaultColor = isDarkTheme ? [255, 255, 255] : [0, 0, 0];
-    const pixels = [];
+    const mountCurrent = mountRef.current;
+    mountCurrent.appendChild(renderer.domElement);
 
+    // Ajout des particules
     for (let x = -1000; x < 1000; x += 10) {
       for (let z = -1000; z < 1000; z += 10) {
-        const zOscillation = Math.sin(z * (Math.PI * 4 / 500));
-        const xOscillation = Math.sin((x + z) * (Math.PI * 2 / 500));
-        const pixel = new Pixel3D(x, (zOscillation + xOscillation) * 14 + 30, z, defaultColor);
-        pixels.push(pixel);
+        const y = (Math.sin(z * (Math.PI * 4 / 500)) + Math.sin((x + z) * (Math.PI * 2 / 500))) * 14 + 30;
+        particles.push(new THREE.Vector3(x, y, z));
       }
     }
 
+    const particleGeometry = new THREE.BufferGeometry().setFromPoints(particles);
+    const particleSystem = new THREE.Points(particleGeometry, particleMaterial);
+    scene.add(particleSystem);
+
+    camera.position.z = 500;
+
+    // Fonction de rendu
     const render = () => {
-      offsetX.current += (mouseX.current - offsetX.current) * 0.1;
-      offsetY.current += (mouseY.current - offsetY.current) * 0.1;
-
-      context.clearRect(0, 0, width, height);
-
-      pixels.forEach(pixel => {
-        const scale = fov / (fov + pixel.z);
-        const x2d = ((pixel.x + offsetX.current) * scale) + halfWidth;
-        const y2d = ((pixel.y + offsetY.current + 30) * scale) + halfHeight;
-        const alpha = 255 - pixel.z - (0.65 * 255);
-        context.fillStyle = `rgba(${pixel.color[0]}, ${pixel.color[1]}, ${pixel.color[2]}, ${alpha / 255})`;
-        context.fillRect(x2d, y2d, 1, 1);
-        pixel.z -= 1;
-        if (pixel.z < -fov) {
-          pixel.z += (fov * 2);
-          pixel.color = isDarkTheme ? [255, 255, 255] : [0, 0, 0];
-        }
-      });
-
       requestAnimationFrame(render);
+      particleSystem.rotation.x += 0.001;
+      particleSystem.rotation.y += 0.001;
+      renderer.render(scene, camera);
     };
 
-    const onMouseMove = (e) => {
-      mouseX.current = (halfWidth - e.clientX) * 0.1;
-      mouseY.current = (halfHeight - e.clientY) * 0.1;
-    };
-
-    document.body.addEventListener("mousemove", onMouseMove);
     render();
 
+    // Nettoyage
     return () => {
-      document.body.removeEventListener("mousemove", onMouseMove);
+      mountCurrent.removeChild(renderer.domElement);
     };
-  }, [width, height, halfWidth, halfHeight, isDarkTheme]);
+  }, [isDarkTheme]); // Ajout de isDarkTheme comme dépendance
 
-  return <canvas ref={canvasRef} />;
+  return <div ref={mountRef} style={{ width: '100%', height: '100%' }}/>;
 };
 
 export default Pixel3DAnimation;
